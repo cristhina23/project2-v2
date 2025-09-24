8ee21fbc47c8823fb125cf58510836beece38f3e
Cristhina <viszellechacon@gmail.com>
2025-09-24 09:49:13 -0500

    upd

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'My Contacts API documentation',
   },
-  host: 'localhost:3000',   
-  schemes: ['http'],        
+  host: 'project2-v2.onrender.com',   
+  schemes: ['https'],        
 };
 
 const outputFile = './swagger.json';
